860b9cba66e61a2c712873ef9927926736b4a8bf
Moon-20070205 <hemu070205@gmail.com>
2025-08-02 14:18:33 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,48 +70,48 @@ export default function Footer() {
           
           {/* 策劃及合辦 */}
           <div className="flex flex-col md:flex-row items-start mb-6 md:mb-0 lg:w-[45%]">
-            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-6 min-w-[5em]">
+            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-4 min-w-[5em]">
               策劃及合辦：
             </div>
-            <div className="flex flex-col w-full space-y-3">
+            <div className="flex flex-col w-full space-y-4"> {/* 增加垂直间距 */}
               <div>
                 <img 
                   src="/img/partners/香港復康會.svg" 
                   alt="香港復康會" 
-                  className="w-full max-w-[280px] md:max-w-[320px] h-auto"
+                  className="w-full max-w-[260px] md:max-w-[300px] h-auto"
                 />
               </div>
-              <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
-                <div className="flex space-x-3">
+              <div className="flex flex-col space-y-4"> {/* 确保垂直排列时不重叠 */}
+                <div className="flex flex-wrap gap-4"> {/* 使用flex-wrap和gap确保不重叠 */}
                   <img 
                     src="/img/partners/生科院3.svg" 
                     alt="生科院3" 
-                    className="w-[70px] md:w-[80px] h-auto"
+                    className="w-[65px] md:w-[75px] h-auto"
                   />
                   <img 
                     src="/img/partners/生科院2.svg" 
                     alt="生科院2" 
-                    className="w-[180px] md:w-[200px] h-auto"
+                    className="w-[160px] md:w-[180px] h-auto"
                   />
                 </div>
                 <img 
                   src="/img/partners/生科院1.svg" 
                   alt="生科院1" 
-                  className="w-[275px] md:w-[300px] h-auto"
+                  className="w-[250px] md:w-[280px] h-auto"
                 />
               </div>
             </div>
           </div>
 
-          {/* 教育夥伴 - 向右移动 */}
+          {/* 教育夥伴 */}
           <div className="flex flex-col md:flex-row items-start lg:w-[45%] ml-4 md:ml-8 lg:ml-12">
-            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-6 min-w-[5em]">
+            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-4 min-w-[5em]">
               教育夥伴：
             </div>
             <img 
               src="/img/partners/善衡書院.svg" 
               alt="善衡書院" 
-              className="w-full max-w-[290px] md:max-w-[330px] h-auto"
+              className="w-full max-w-[270px] md:max-w-[310px] h-auto"
             />
           </div>
         </div>
@@ -121,25 +121,25 @@ export default function Footer() {
           
           {/* 資助 */}
           <div className="flex flex-col md:flex-row items-start mb-6 md:mb-0 lg:w-[45%]">
-            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-6 min-w-[5em]">
+            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-4 min-w-[5em]">
               資助：
             </div>
             <img 
               src="/img/partners/研究及知識轉移處.svg" 
               alt="研究及知識轉移處" 
-              className="w-full max-w-[290px] md:max-w-[330px] h-auto"
+              className="w-full max-w-[270px] md:max-w-[310px] h-auto"
             />
           </div>
 
-          {/* 協力 - 向右移动 */}
+          {/* 協力 */}
           <div className="flex flex-col md:flex-row items-start lg:w-[45%] ml-4 md:ml-8 lg:ml-12">
-            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-6 min-w-[5em]">
+            <div className="text-gray-800 text-base md:text-lg font-light mb-3 md:mb-0 md:mr-4 min-w-[5em]">
               協力：
             </div>
             <img 
               src="/img/partners/小腦萎縮症協會.svg" 
               alt="小腦萎縮症協會" 
-              className="w-full max-w-[220px] md:max-w-[250px] h-auto"
+              className="w-full max-w-[200px] md:max-w-[230px] h-auto"
             />
           </div>
         </div>
