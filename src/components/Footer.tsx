6feafb66710dfd383853d84eb888e51b31b62040
Moon-20070205 <hemu070205@gmail.com>
2025-08-02 14:05:31 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,64 +63,84 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="relative w-full h-[377px] bg-white" id="partners-section">
-      {/* 背景底色 */}
-      <div className="absolute bg-white h-full w-full" />
-      
-      {/* 香港復康會 */}
-      <div className="absolute h-[138.564px] left-[220.18px] top-[30.72px] w-[355.687px]">
-        <img src="/img/partners/香港復康會.svg" alt="香港復康會" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 生科院1 */}
-      <div className="absolute h-[84.293px] left-[648.64px] top-[94.35px] w-[344.439px]">
-        <img src="/img/partners/生科院1.svg" alt="生科院1" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 生科院2 */}
-      <div className="absolute h-[80.829px] left-[759.42px] top-[34.59px] w-[225.274px]">
-        <img src="/img/partners/生科院2.svg" alt="生科院2" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 生科院3 */}
-      <div className="absolute h-[80.829px] left-[651.84px] top-[34.59px] w-[92.696px]">
-        <img src="/img/partners/生科院3.svg" alt="生科院3" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 善衡書院 */}
-      <div className="absolute h-[118.934px] left-[1470.4px] top-[27.03px] w-[367.195px]">
-        <img src="/img/partners/善衡書院.svg" alt="善衡書院" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 研究及知識轉移處 */}
-      <div className="absolute h-[110.851px] left-[208.96px] top-[215.57px] w-[365.809px]">
-        <img src="/img/partners/研究及知識轉移處.svg" alt="研究及知識轉移處" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 小腦萎縮症協會 */}
-      <div className="absolute h-[173.205px] left-[1517.81px] top-[177.4px] w-[273.322px]">
-        <img src="/img/partners/小腦萎縮症協會.svg" alt="小腦萎縮症協會" className="w-full h-full object-contain" />
-      </div>
-      
-      {/* 策劃及合辦 */}
-      <div className="absolute font-light left-[36.67px] text-black text-[24px] top-[62px]">
-        <p>策劃及合辦：</p>
-      </div>
-      
-      {/* 教育夥伴 */}
-      <div className="absolute font-light left-[1310px] text-black text-[24px] top-[62px]">
-        <p>教育</p>
-        <p>夥伴：</p>
-      </div>
-      
-      {/* 資助 */}
-      <div className="absolute font-light left-[36.67px] text-black text-[24px] top-[268px]">
-        <p>資助：</p>
-      </div>
-      
-      {/* 協力 */}
-      <div className="absolute font-light left-[1310px] text-black text-[24px] top-[268px]">
-        <p>協力：</p>
+    <div className="w-full bg-white py-12 lg:py-16">
+      <div className="container mx-auto px-4">
+        {/* 第一行：策劃及合辦 + 教育夥伴 */}
+        <div className="flex flex-col lg:flex-row justify-between items-start mb-12 lg:mb-16">
+          {/* 策劃及合辦 */}
+          <div className="flex flex-col lg:flex-row items-start mb-8 lg:mb-0 lg:w-[45%]">
+            <div className="text-gray-800 text-lg lg:text-2xl font-light mb-4 lg:mb-0 lg:mr-8 min-w-[6em]">
+              策劃及合辦：
+            </div>
+            <div className="flex flex-col w-full">
+              <div className="mb-4">
+                <img 
+                  src="/img/partners/香港復康會.svg" 
+                  alt="香港復康會" 
+                  className="w-full max-w-[355px] h-auto object-contain"
+                />
+              </div>
+              <div className="flex flex-col lg:flex-row gap-4">
+                <div className="flex gap-4">
+                  <img 
+                    src="/img/partners/生科院3.svg" 
+                    alt="生科院3" 
+                    className="w-[92px] lg:w-[100px] h-auto object-contain"
+                  />
+                  <img 
+                    src="/img/partners/生科院2.svg" 
+                    alt="生科院2" 
+                    className="w-[225px] lg:w-[240px] h-auto object-contain"
+                  />
+                </div>
+                <img 
+                  src="/img/partners/生科院1.svg" 
+                  alt="生科院1" 
+                  className="w-[344px] lg:w-[360px] h-auto object-contain"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* 教育夥伴 */}
+          <div className="flex flex-col lg:flex-row items-start lg:w-[45%]">
+            <div className="text-gray-800 text-lg lg:text-2xl font-light mb-4 lg:mb-0 lg:mr-8 min-w-[6em]">
+              教育夥伴：
+            </div>
+            <img 
+              src="/img/partners/善衡書院.svg" 
+              alt="善衡書院" 
+              className="w-full max-w-[367px] h-auto object-contain"
+            />
+          </div>
+        </div>
+
+        {/* 第二行：資助 + 協力 */}
+        <div className="flex flex-col lg:flex-row justify-between items-start">
+          {/* 資助 */}
+          <div className="flex flex-col lg:flex-row items-start mb-8 lg:mb-0 lg:w-[45%]">
+            <div className="text-gray-800 text-lg lg:text-2xl font-light mb-4 lg:mb-0 lg:mr-8 min-w-[6em]">
+              資助：
+            </div>
+            <img 
+              src="/img/partners/研究及知識轉移處.svg" 
+              alt="研究及知識轉移處" 
+              className="w-full max-w-[365px] h-auto object-contain"
+            />
+          </div>
+
+          {/* 協力 */}
+          <div className="flex flex-col lg:flex-row items-start lg:w-[45%]">
+            <div className="text-gray-800 text-lg lg:text-2xl font-light mb-4 lg:mb-0 lg:mr-8 min-w-[6em]">
+              協力：
+            </div>
+            <img 
+              src="/img/partners/小腦萎縮症協會.svg" 
+              alt="小腦萎縮症協會" 
+              className="w-full max-w-[273px] h-auto object-contain"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
