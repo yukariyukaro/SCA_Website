c049014642d6e7ca260c2859fe5b2b43bbac254b
jiaohaonan <jiaohaonan@bytedance.com>
2025-10-21 21:54:39 +0800

    feat: add locator js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react({
+      babel: {
+        plugins: [
+          // other Babel plugins
+          [
+            "@locator/babel-jsx/dist",
+            {
+              env: "development",
+            },
+          ],
+        ],
+      },
+    }), tailwindcss()],
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
